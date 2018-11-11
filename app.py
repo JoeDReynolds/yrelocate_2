7246e54360b6ef36979504ef54bd84d628a818f2
Bill Frye <3730304+billfrye@users.noreply.github.com>
2018-11-11 15:26:43 -0600

    Initial Bill01 changes

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-import os
-from flask import Flask, jsonify, render_template, url_for
+# import os
+# from flask import Flask, jsonify, render_template, url_for
 import json
 from bson import json_util
-from bson.json_util import dumps
+from flask import Flask, render_template
+# from bson.json_util import dumps
 from flask_pymongo import PyMongo
 
-from models import retrive_population_data, insert_population_data
+from models import retrive_population_data
 
-##############################################################################
+# ########################################################################
 
 app = Flask(__name__)
 
@@ -15,27 +16,29 @@ app.config["MONGO_URI"] = "mongodb://localhost:27017/yrelocate_db"
 mongo = PyMongo(app)
 
 
-##### Define routes #####################################################################
+# Define routes ###############################################################
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/population")   
+
+@app.route("/population")
 def population():
     print("---------------population-----------------")
-    # Get the Data from MongoDc
+    # Get the Data from mongodb
     projects = retrive_population_data(mongo)
     json_projects = []
     for project in projects:
         json_projects.append(project)
     json_projects = json.dumps(json_projects, default=json_util.default)
-    return json_projects    
+    return json_projects
 
 
-################################################################################
-################################################################################
+# ###########################################################################
+# ################################3##########################################
+
 if __name__ == '__main__':
-    app.run(debug=True)    
+    app.run(debug=True)
 
 
 # insert_population_data(mongo)
