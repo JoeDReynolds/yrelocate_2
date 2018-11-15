9a8e9cff4779aacc2e35fe45006bcf9ebb187f12
maryam A.Samani <maryam.smni@gmail.com>
2018-11-14 22:38:13 -0600

    remove unnecessary staff

--- home page /app.py
+++ home page /app.py
@@ -11,7 +11,7 @@ from models import retrieve_population_data, insert_population_data,retrieve_Pho
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/yrelocate_db"
+app.config["MONGO_URI"] = "mongodb://joe_reynolds:op3nupd4n@ds155903.mlab.com:55903/heroku_j29mjxk2"
 mongo = PyMongo(app)
 
 
@@ -31,6 +31,7 @@ def population():
     json_projects = json.dumps(json_projects, default=json_util.default)
     return json_projects   
 #  retrive best Photos for citys
+
 @app.route("/Photos") 
 def Photos():
     print("---------------Photos-----------------")
@@ -46,6 +47,3 @@ def Photos():
 ################################################################################
 if __name__ == '__main__':
     app.run(debug=True)    
-
-
-# insert_population_data(mongo)
